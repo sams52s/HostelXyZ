ef44d5548173948927930a0dd6242d4e1906b005
Toufiq <55557644+sams52s@users.noreply.github.com>
2022-06-13 19:00:54 +0600

    Login UI Created

--- HostelXyZ/HostelXyZ/App.xaml.cs
+++ HostelXyZ/HostelXyZ/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HostelXyZ.LoginRegistration.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace HostelXyZ
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginView();
         }
 
         protected override void OnStart()
--- HostelXyZ/HostelXyZ/AssemblyInfo.cs
+++ HostelXyZ/HostelXyZ/AssemblyInfo.cs
@@ -1,3 +1,10 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("SF.ttf", Alias = "SF")]
+[assembly: ExportFont("SFBlack.ttf", Alias = "SFBlack")]
+[assembly: ExportFont("SFBoldItalic.ttf", Alias = "SFBoldItalic")]
+[assembly: ExportFont("SFExtraLight.ttf", Alias = "SFExtraLight")]
+[assembly: ExportFont("SourceSansPro-SemiBold.ttf", Alias = "SFSemiBold")]
